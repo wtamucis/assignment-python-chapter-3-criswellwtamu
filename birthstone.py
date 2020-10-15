46f5354ecc93f62e785786953563708d2d5697f0
criswellwtamu <tjcriswell1@buffs.wtamu.edu>
2020-10-14 19:23:29 -0500

    first commit attempt

--- birthstone.py
+++ birthstone.py
@@ -1,2 +1,29 @@
 # Follow the assignment instructions to code an app that
 # will tell a user their birthstone.
+print("Enter the number of the month you were born (1-12)")
+user_input = 1
+birthstone = 8
+if birthstone == 1:
+    print("Your birthstone is garnet")
+elif birthstone == 2:
+    print("Your birthstone is amethyst")
+elif birthstone == 3:
+    print("Your birthstone is aquamarine")
+elif birthstone == 4:
+    print("Your birthstone is diamond")
+elif birthstone == 5:
+    print("Your birthstone is emerald")
+elif birthstone == 6:
+    print("Your birthstone is pearl")
+elif birthstone == 7:
+    print("Your birthstone is ruby")
+elif birthstone == 8:
+    print("Your birthstone is peridot")
+elif birthstone == 9:
+    print("Your birthstone is sapphire")
+elif birthstone == 10:
+    print("Your birthstone is opal")
+elif birthstone == 11:
+    print("Your birthstone is topaz")
+elif birthstone == 12:
+    print("Your birthstone is turquoise")
